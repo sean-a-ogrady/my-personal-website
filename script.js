8beb8074b2675a6aa0cfc91d7b95cf8877791bab
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-08-31 04:46:00 -0400

    Add funny script

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This is content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const bottomLeftArea = document.getElementById("bottom-left-area");
+let alertTimes = 0;
+function clickAlert(){
+    alertTimes++;
+    switch (alertTimes) {
+        case 1:
+            alert("alright listen I promise I'll put something here just hold on");
+            break;
+        case 2:
+            alert("cmon man I just told you I'll do something here, chill");
+            break;
+        case 3:
+            alert("dude how many times I gotta tell you, cut it out bro")
+            break;
+        default:
+            alert(">:(");
+            break;
+    }
+}
+bottomLeftArea.addEventListener('click', clickAlert);
