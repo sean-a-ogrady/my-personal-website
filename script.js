3020fab5a83fe8d0b0c12a723b6e64ae21492b36
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-09-28 19:27:41 -0400

    Tweak word scaling and style

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ fetch(url)
 // Initialize word cloud
 function initializeWordCloud(words) {
 
+    // Start with a base size
+    let baseSize = 10;
+
+    // Calculate initial scaling factor (adjustible)
+    let scalingFactor = 0.5 * Math.sqrt(width * height / words.length);
+
     const svg = d3.select("#word-cloud-container").append("svg")
         .attr("width", "100%")
         .attr("height", "100%")
@@ -29,34 +35,46 @@ function initializeWordCloud(words) {
     // Define color scales for sentiment
     const colorScalePositive = d3.scaleLinear()
         .domain([0, 5])
-        .range(["#d3d3d3", "green"]);
+        .range(["#6EBBCD", "green"]);
 
     const colorScaleNegative = d3.scaleLinear()
         .domain([-5, 0])
-        .range(["red", "#d3d3d3"]);
+        .range(["red", "#6EBBCD"]);
+
+    // Sort words by frequency so that largest words are drawn first
+    words = words.sort((a, b) => b.frequency - a.frequency);
 
     const layout = d3.layout.cloud()
         .size([width, height])
-        .words(words.map(word => {
-            return { text: word.word, size: Math.sqrt(word.frequency) * 20, sentiment: word.sentiment };
+        .words(words.map((word, i) => {
+            // Dynamic scaling based on frequency and position in the array
+            let dynamicScaling = scalingFactor * (1 - i / words.length);
+            // Use the scaling factor to adjust the size
+            return { text: word.word, size: baseSize + Math.sqrt(word.frequency) * dynamicScaling, sentiment: word.sentiment };
         }))
-        .padding(5)
+        .padding(6)
         .rotate(() => {
             const angles = [0, 90, 270];
             return angles[Math.floor(Math.random() * 3)];
         })
+        .text(function (d) { return d.text; })
+        .font("AndaleMono")
         .fontSize(d => d.size)
         .on("end", draw);
 
     layout.start();
 
     function draw(words) {
+        const paddingX = 30; // Padding from the right
+        const paddingY = 30; // Padding from the bottom
+
         svg.append("g")
-            .attr("transform", `translate(${pageWidth / 2},${pageHeight / 2})`)
+            .attr("transform", `translate(${pageWidth / 2 - paddingX},${pageHeight / 2 - paddingY})`)
             .selectAll("text")
             .data(words)
             .enter().append("text")
             .style("font-size", d => d.size + "px")
+            .style("opacity", 0.1)
             .style("fill", d => {
                 // Change color based on sentiment
                 if (d.sentiment > 0) {
@@ -64,7 +82,7 @@ function initializeWordCloud(words) {
                 } else if (d.sentiment < 0) {
                     return colorScaleNegative(d.sentiment);
                 } else {
-                    return "#d3d3d3";  // Grey for neutral sentiment
+                    return "#6EBBCD";  // Grey for neutral sentiment
                 }
             })
             .attr("text-anchor", "middle")
